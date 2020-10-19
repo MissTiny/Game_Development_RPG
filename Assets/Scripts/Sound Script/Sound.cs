092a91f5a630c7247872870719efdf63ff63b152
Miss Tiny <jingyi-yang@uiowa.edu>
2020-10-18 21:53:48 -0500

    Sound & scene Manager

---
+++ Assets/Scripts/Sound Script/Sound.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound{
+
+    // Start is called before the first frame update
+    public AudioClip clip;
+    public string name;
+
+    [Range(0f,1f)]
+    public float volume;
+    [Range(.1f,3f)]
+    public float pitch;
+
+    public bool loop = false;
+
+    [HideInInspector]
+    public AudioSource source;
+}
