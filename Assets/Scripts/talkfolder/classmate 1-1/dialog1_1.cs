eb567c40addb357f21b0575675e06d1faf7f6416
deathlord <zhangbohan039@gmail.com>
2020-10-19 00:03:45 -0500

    add all talkbox

---
+++ Assets/Scripts/talkfolder/classmate 1-1/dialog1_1.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class dialog1_1 : MonoBehaviour
+{
+    [Header("UI")]
+    public Text textlabel;
+
+    [Header("textfile")]
+    public TextAsset txtfile;
+    public int index;
+
+
+    List<string> textlist = new List<string>();
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Gettextfromfile(txtfile);
+        
+    }
+    private void OnEnable()
+    {
+        textlabel.text = textlist[index];
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if((Input.GetKeyDown(KeyCode.F))&& index == textlist.Count)
+        {
+            gameObject.SetActive(false);
+            index = 0;
+            return;
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            textlabel.text = textlist[index];
+            index++;
+        }
+    }
+    private void OntriggerExit2D(Collider2D other)
+    {
+       
+    }
+    void Gettextfromfile(TextAsset file)
+    {
+        textlist.Clear();
+        index = 0;
+        //file.text.Split('\n');
+        var linedata = file.text.Split('\n');
+        foreach (var line in linedata)
+        {
+            textlist.Add(line);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/talkfolder/classmate 1-1/starttalk1_1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starttalk1_1 : MonoBehaviour
+{
+    public GameObject Button;
+    public GameObject talkUI;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Button.SetActive(true);
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Button.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Button.activeSelf && Input.GetKeyDown(KeyCode.F))
+        {
+            talkUI.SetActive(true);
+        }
+    }
+}
