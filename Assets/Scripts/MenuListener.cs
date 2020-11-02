c3e2def9727b52efe418d0bda0f274e3aa266220
Zhangsiyu <zhangsiyu1107@gmail.com>
2020-11-02 06:51:39 -0600

    SubMenu

---
+++ Assets/Scripts/MenuListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuListener : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject submenu;
+    private bool submenu_state = false;
+    void Start()
+    {
+        submenu.SetActive(submenu_state);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && submenu_state == false)
+        {
+            submenu_state = !submenu_state;
+            submenu.SetActive(submenu_state);
+        }else if(Input.GetKeyDown(KeyCode.Escape) && submenu_state == true)
+        {
+            submenu_state = !submenu_state;
+            submenu.SetActive(submenu_state);
+        }
+    }
+}
