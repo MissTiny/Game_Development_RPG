c4ded1fe14c8165706c70dc7cdbef21d216f372d
Unknown <unknown@example.com>
2020-11-14 18:23:56 -0600

    Add Bag_panel and Equipment panel, player
    can press i, and e to open them(Cannot interact yet)

---
+++ Assets/Inventory/ShowEquip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowEquip : MonoBehaviour
+{
+    public GameObject myEquip;
+    
+    private bool open;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OpenBag();
+    }
+    void OpenBag()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            open = myEquip.active;
+            open = !open;
+            myEquip.SetActive(open);
+        }
+    }
+}
