5f1a967caeba54ecff8b7ea300a4a092bd860e98
Zhangsiyu <zhangsiyu1107@gmail.com>
2020-11-27 14:51:32 -0600

    对话模块

---
+++ Assets/Scripts/Character_list.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_list : MonoBehaviour
+{
+    public List<Charater> charaters;
+    
+    public Character_list()
+    {
+        charaters.Add(new Charater("Emma", new List<string> { "Hello! What's up!"}, new Dictionary<string, int> { { "Book", 5 } }));
+    }
+
+   
+}
